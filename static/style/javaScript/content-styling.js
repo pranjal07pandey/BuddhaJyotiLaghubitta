ec096a51d4dd7b7574abf290fc254a5007fde1e4
pranjal07pandey <pranjalpandey92@gmail.com>
2019-07-06 09:16:01 +0545

    changed some css

--- static/style/javaScript/content-styling.js
+++ static/style/javaScript/content-styling.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $( "#navbar").load( "../navigation/navbar.html", function() {
         console.log("Navbar loaded");
-        // let navHeight = $('.navigation-clean').outerHeight();
-        // console.log(navHeight);
-        // $('.content').css({'margin-bottom': navHeight});
+        let navHeight = $('.navigation-clean').outerHeight();
+        console.log(navHeight);
+        $('.content').css({'margin-bottom': navHeight});
     });
 
     $("#footer").load("../footer/footer.html", function () {
